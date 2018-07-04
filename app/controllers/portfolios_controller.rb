7fb034c494612eedaae68c42712bf53b74fda637
petar <prlinap@gmail.com>
2018-07-04 11:14:48 +0200

    Setting up a edit page for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,6 +16,21 @@ class PortfoliosController < ApplicationController
   end
 end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    respond_to do |format|
+      @portfolio_item = Portfolio.find(params[:id])
+      if @portfolio_item.update_attributes(portfolio_params)
+        format.html {redirect_to portfolios_path, notice: 'Portfolio was updated successfully'}
+      else
+        format.html { render :edit}
+      end
+    end
+  end
+
 
   private
   def portfolio_params
