8f67e1e6009fbc290a522f095b40bea0814af533
petar <prlinap@gmail.com>
2018-07-05 12:17:12 +0200

    Implemented validations for blogs, skills and portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,6 @@
 class Portfolio < ApplicationRecord
+  validates :title, :body, :main_image, :thumb_image, presence:true
+  # validates :body, presence:true
+  # validates :main_image, presence:true
+  # validates :thumb_image, presence:true
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,4 @@
 class Skill < ApplicationRecord
+  validates :title,:percent_utilized, present:true
+  # validates :percent_utilized, present:true
 end
