43262db032d4b0b248da937e4cde4f8de4e7b599
petar <prlinap@gmail.com>
2018-07-09 20:12:52 +0200

    Implemented authorization engine for portfolio items and blogs

--- app/models/concerns/current_user_concern.rb
+++ app/models/concerns/current_user_concern.rb
@@ -1,5 +1,6 @@
 module CurrentUserConcern
   extend ActiveSupport::Concern
+  
   def current_user
     super || guest_user
   end
@@ -9,6 +10,12 @@ module CurrentUserConcern
   #super - poziva metod sa istim nazivom u parent klasi 
 
   def guest_user
-    OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
+    # OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
+    guest = GuestUser.new
+    guest.name = "Guest User"
+    guest.first_name = "Guest"
+    guest.last_name = "User"
+    guest.email = "guest@example.com"
+    guest
   end
 end
