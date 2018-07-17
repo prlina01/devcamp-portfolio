5ee82d763449d8c9ca00166999fc135aa0f79d64
petar <prlinap@gmail.com>
2018-07-17 19:14:53 +0200

    Integrated initial drag and drop implementation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
 //= require jquery-ui
 //= require activestorage
 //= require html.sortable
 //= require turbolinks
-//= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 //= require_tree .
